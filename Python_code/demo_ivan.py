9fd2977c000bc691b90270ac33d886eead8e68d7
Ivan Gonzalez <ivanesmexicano@gmail.com>
2024-08-13 19:09:45 -0600

    just demo

---
+++ Python_code/demo_ivan.py
@@ -0,0 +1,25 @@
+#demo ivan
+
+def upload_video(directory, video_name):
+    """
+    Uploads a video from a specific directory.
+
+    Args:
+        directory (str): The path to the directory containing the video to be uploaded.
+        video_name (str): The name of the video to be uploaded.
+
+    Returns:
+        bool: True if the video is uploaded successfully, False otherwise.
+    """
+
+    # Add code here to upload the video
+    directory="c/Users/cochigordo/video_demo"
+    video_name="dance_seq.mp4"
+
+    return True
+
+upload_video("/home/ivan/Downloads", "ivan.mp4")
+
+    
+
+print(upload_video)
